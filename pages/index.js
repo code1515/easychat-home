e5d61df6dbe116e3afd654d2e4809a1bff1ed96f
liuyuhao <liuyuhao@doodod.com>
2024-11-11 13:01:43 +0800

    google analysis

--- pages/index.js
+++ pages/index.js
@@ -13,16 +13,17 @@ export default function Home() {
     <>
       <Head>
         {/*<meta name='robots' content='index' />*/}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-21QW02NCV4"></script>
-          <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments)}
-              gtag('js', new Date());
-
-              gtag('config', 'G-21QW02NCV4');
-          </script>
         <meta name="google-site-verification" content="QbP4MzNmvuKgFZhZ9EU2_cJD9PTd1OWNplhmdvHGUGU" />
       </Head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-21QW02NCV4"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-21QW02NCV4');
+        </script>
       <Hero />
       <LogoGrid />
       <GradientWrapper>
